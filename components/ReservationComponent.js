ad5a8e9120029e5cd8dcd30726b60b58fe4efeee
Jeremy Verstegen <jvmedia77@gmail.com>
2022-06-02 13:32:44 -0600

    Local Notification

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -29,6 +30,31 @@ class Reservation extends Component {
         });
     }
 
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
+    }
+
     handleReservation() {
             Alert.alert(
                 'Begin Search? \n\n',
@@ -39,11 +65,17 @@ class Reservation extends Component {
                     {
                         text: 'Cancel',
                         style: 'cancel',
-                        onPress: () => console.log('Canceled Search.')
+                        onPress: () => {
+                            console.log('Canceled Search.');
+                            this.resetForm();
+                        }
                     },
                     {
                         text: 'OK',
-                        onPress: () => console.log('Reservation Submitted.')
+                        onPress: () => {
+                            this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                            this.resetForm();
+                        }
                     },
                 ],
                 {cancelable: false}
